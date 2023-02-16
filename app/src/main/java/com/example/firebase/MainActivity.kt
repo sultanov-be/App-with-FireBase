bcfe2ddefe9f37f839069c865ca55533608b1a52
bektur.sultanov <bektur.sultanov@myworkflow.ru>
2023-02-16 10:48:50 +0600

    Add Navigation.

--- app/src/main/java/com/example/firebase/MainActivity.kt
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupNavController()
+    }
+
+    private fun setupNavController() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        navController.graph = navController.navInflater.inflate(R.navigation.main_graph)
     }
 }
