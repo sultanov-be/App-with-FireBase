03e8d1362b21a64270fc02a40cd2ee1a65ca0614
bektur.sultanov <bektur.sultanov@myworkflow.ru>
2023-02-16 10:39:19 +0600

    Base commit.

---
+++ app/src/main/java/com/example/firebase/ui/MainFragment.kt
@@ -0,0 +1,26 @@
+package com.example.firebase.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.firebase.databinding.FragmentMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainFragment : Fragment() {
+    private val viewModel: MainViewModel by viewModels()
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(layoutInflater)
+        viewModel
+
+        return binding.root
+    }
+}
