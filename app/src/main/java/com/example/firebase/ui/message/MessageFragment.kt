93847088f4a1d61e02765f22c0378f6c93d618cf
bektur.sultanov <bektur.sultanov@myworkflow.ru>
2023-02-17 12:02:01 +0600

    Made UI of msg fragment, sendMessage().

--- app/src/main/java/com/example/firebase/ui/message/MessageFragment.kt
+++ app/src/main/java/com/example/firebase/ui/message/MessageFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import com.example.firebase.R
 import com.example.firebase.databinding.FragmentMessageBinding
 
 class MessageFragment : Fragment() {
@@ -21,9 +25,38 @@ class MessageFragment : Fragment() {
         binding = FragmentMessageBinding.inflate(layoutInflater)
 
         args = MessageFragmentArgs.fromBundle(requireArguments())
-
-        binding.id.text = args!!.id
+        viewModel.getData(args!!.id)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding) {
+        super.onViewCreated(view, savedInstanceState)
+
+        sendBtn.setOnClickListener {
+            if (emailInputText.text.toString() != "") {
+                //TODO(send message)
+            }
+        }
+
+        showNickname()
+    }
+
+    private fun showNickname() = with(binding) {
+        viewModel.user.observe(viewLifecycleOwner) {
+            if (it == null) {
+                Toast.makeText(requireContext(), "User not found", Toast.LENGTH_SHORT).show()
+            } else {
+                username.text = getString(R.string.user, it.username)
+                showViews()
+            }
+        }
+    }
+
+    private fun showViews() = with(binding) {
+        progressbar.visibility = GONE
+        tb.visibility = VISIBLE
+        emailInputLayout.visibility = VISIBLE
+        sendBtn.visibility = VISIBLE
     }
 }
--- app/src/main/java/com/example/firebase/ui/message/MessageViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/message/MessageViewModel.kt
@@ -1,7 +1,48 @@
 package com.example.firebase.ui.message
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.firebase.firebase.User
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MessageViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val user = MutableLiveData<User?>()
+
+    fun getData(id: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val reference = FirebaseDatabase.getInstance().getReference("Users").child(id)
+
+            reference.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val user1: User? = dataSnapshot.getValue(User::class.java)
+
+                    if (user1 != null) {
+                        user.value = user1
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                }
+            })
+        }
+    }
+
+    fun sendMessage(msg: String, from: String, to: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val reference = FirebaseDatabase.getInstance().reference
+
+            val message = HashMap<String, Any>()
+            message["sender"] = to
+            message["receiver"] = from
+            message["message"] = msg
+
+            reference.child("Chats").push().setValue(message)
+        }
+    }
 }
